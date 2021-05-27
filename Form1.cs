20c558d14ac4247c8dc1fb072806e82f265f44e4
NikosSalichos <70713775+Nikos-Salichos@users.noreply.github.com>
2021-05-27 12:16:21 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Calculate calculate = new Calculate();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double result = calculate.Sum(double.Parse(textBox1.Text), double.Parse(textBox2.Text));
+                textBox3.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double result = calculate.Subtraction(double.Parse(textBox1.Text), double.Parse(textBox2.Text));
+                textBox3.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double result = calculate.Multiplication(double.Parse(textBox1.Text), double.Parse(textBox2.Text));
+                textBox3.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Models/Calculate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class Calculate
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
